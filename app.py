89550428386ba75b82cc36247539694c77ba6358
MarcoZinzeri <marksnine9007@gmail.com>
2024-12-17 11:47:19 +0000

    esercizio 4

--- app.py
+++ app.py
@@ -1,6 +1,17 @@
 from flask import Flask
+from flask_mysql import MySQL
 
 app = Flask(__name__)
+
+app.config['MYSQL_HOST']="138.41.20.102"
+app.config['MYSQL_PORT']="53306"
+app.config['MYSQL_DB']="w3schools"
+app.config['MYSQL_USER']="ospite"
+app.config['MYSQL_PASSWORD']="ospite"
+
+cursor=mysql.connection.cursor()
+query='SELECT '
+cursor.execute(query)
 
 @app.route("/")
 def hello_world():
